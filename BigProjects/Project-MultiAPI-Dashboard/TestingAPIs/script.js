2711f4488785914ca34f0ccdf4912967973ea353
VictorFrias25 <victorfriaz11@gmail.com>
2025-02-27 17:59:20 -0500

    added google api to gitignore

---
+++ BigProjects/Project-MultiAPI-Dashboard/TestingAPIs/script.js
@@ -0,0 +1,22 @@
+import { googleAPI } from './constants'
+alert(`test`)
+const CONFIGURATION = {
+    "locations": [
+      {"title":"FerminFadez Barbershop","address1":"601 N 15th St","address2":"Immokalee, FL 34142, USA","coords":{"lat":26.42781141907233,"lng":-81.43426050552253},"placeId":"ChIJQ_yWxSCn24gRxWBfVF2wmMQ"}
+    ],
+    "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17,"mapId":""},
+    "mapsApiKey": googleAPI,
+    "capabilities": {"input":false,"autocomplete":false,"directions":false,"distanceMatrix":false,"details":false,"actions":false}
+  };
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//     await customElements.whenDefined('gmpx-store-locator');
+//     const locator = document.querySelector('gmpx-store-locator');
+//     locator.configureFromQuickBuilder(CONFIGURATION);
+//   });
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await customElements.whenDefined('gmpx-store-locator');
+    const locator = document.querySelector('gmpx-store-locator');
+    locator.configureFromQuickBuilder(CONFIGURATION);
+  });
